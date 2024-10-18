9ed310521b75d635c7e668c073e74d88d676b85b
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2024-10-18 18:31:34 +0530

    exception run succesfully

--- src/exception.py
+++ src/exception.py
@@ -6,40 +6,43 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.logger import logging
 
-def error_message_detail(error):
-    """Extract detailed error message and traceback."""
-    _, _, exc_tb = sys.exc_info()  # Get the current exception info
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in script [{0}] at line [{1}]: {2}".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+# def error_message_detail(error):
+#     """Extract detailed error message and traceback."""
+    # _, _, exc_tb = sys.exc_info()  # Get the current exception info
+    # file_name = exc_tb.tb_frame.f_code.co_filename
+    # error_message = "Error occurred in script [{0}] at line [{1}]: {2}".format(
+    #     file_name, exc_tb.tb_lineno, str(error)
+    # )
+    # return error_message
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error)
+     )
     return error_message
-# def error_message_detail(error,error_detail:sys):
-#     _,_,exc_tb=error_detail.exc_info()
-#     file_name=exc_tb.tb_frame.f_code.co_filename
-#     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-#      file_name,exc_tb.tb_lineno,str(error))
-
-#     return error_message
 
     
 
 class CustomException(Exception):
-    # def __init__(self,error_message,error_detail:sys):
-        # super().__init__(error_message)
-        # self.error_message=error_message_detail(error_message,error_detail=error_detail)
-    def __init__(self, error_message, error):
+    def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error)
-
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
     
 
+   # def __init__(self, error_message, error):
+    #     super().__init__(error_message)
+    #     self.error_message = error_message_detail(error)
+
+
+
 
 if __name__=="__main__":
     try:
-        a = 1 / 0  
+        a = 1/0  
     except ZeroDivisionError as e:
         logging.info("Divide by Zero")
         raise CustomException(e,sys)
