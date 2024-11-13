ffd01fdd0a36b51f91ebf05fc8fc38e7a798fef7
Abhinavsaini112 <abhinavsaini8439@gmail.com>
2024-11-13 22:51:37 +0530

    data_transformation

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,7 +46,7 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+                                             models=models)
         
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
